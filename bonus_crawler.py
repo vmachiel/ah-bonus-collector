81b21458ffaa8593c93878d1ccef972cc729021f
mengqi <melody2014ymq@gmail.com>
2017-01-10 21:37:45 +0100

    comments added

--- bonus_crawler.py
+++ bonus_crawler.py
@@ -26,11 +26,16 @@ class BonusCrawler:
     try:
       self.driver.get(url)
       WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, '//body/div/div/div/div/article[last()]')))      
+      
+      #get html
       element = self.driver.find_element_by_xpath('//*')
       html = element.get_attribute('outerHTML') 
+       
+      #parse html and find elements  
       parser = etree.HTMLParser()
       self.html_tree = etree.parse(StringIO(html), parser)
       li = self.html_tree.xpath('//div/div/div/div/article')
+      
       for prod in li:
         p = prod.find('./a')
         if p is not None:
@@ -48,8 +53,10 @@ class BonusCrawler:
               product['price_after'] = None	
             product['discount'] = p.find('./div[2]/span').text
             self.products[name] = product
+    
     except Exception as e:
       print ('An error occured: {0}'.format(e))
+    
     return self.products
 
   #keep crawling sub-pages
@@ -58,11 +65,15 @@ class BonusCrawler:
     for url in self.urls:
       self.getprodinfo(url)
       urls_to_be_extracted = []
+
+      #navigate to the element including further url paths
       li = self.html_tree.xpath('//div/div/div/div/article/div/a')
       urls_to_be_extracted = ['https://www.ah.nl' + ele.attrib['href'] for ele in li]
       urls_temp = urls_to_be_extracted
       self.urls = urls_temp
       self.urls_all.extend(urls_temp)
+
+    #crawl more or terminate
     if not urls_temp:
       self.driver.quit()
       return self.urls_all
